fd052b3a25a6bed8ac636e389300cd6eaaa3d852
adibjabrah <adibaljabrah@gmail.com>
2026-03-09 10:17:08 +0700

    first commit

---
+++ app/src/main/java/com/example/nabilapps/pertemuan_2/BelajarKotlin.kt
@@ -0,0 +1,44 @@
+package com.example.nabilapps.pertemuan_2
+
+fun main() {
+    println("Hai Rekan-rekan...")
+    println("Selamat Datang di bahasa pemograman Kotlin ")
+
+    println("===============")
+
+    var angka = 15
+    println("Hasil dari 154 + 10 = ${angka + 10}")
+
+    val  nilaiInt = 100000
+    val  nilaiDouble = 100.003
+    val  nilaiFloat = 1000.0f
+
+    println("Nilai Integer = $nilaiInt")
+    println("Nilai Double = $nilaiDouble")
+    println("Nilai Float = $nilaiFloat")
+
+    println("====== STRING ======")
+    val huruf = 'a'
+    println("ini penggunaan karakter '$huruf'")
+
+    val nilaiString = "Mawar"
+    println("Helo $nilaiString!\nApa Kabar")
+
+    println("===== Kondisi =====")
+    val nilai = 10
+    if(nilai<0)
+        println("Bilangan negatif")
+    else {
+        if(nilai%2 == 0)
+            println("Bilangan Genap")
+        else
+            println("Bilangan Ganjil")
+    }
+
+    println("===== PERULANGAN =====")
+    val kampusKu: Array<String> = arrayOf("kampus","Politeknik","Caltex","Riau")
+    for (kampus: String in kampusKu){
+        println(kampus)
+    }
+
+}
