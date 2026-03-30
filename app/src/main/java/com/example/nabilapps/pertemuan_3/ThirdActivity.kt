902457213e28a4413d39e62d8525ec4b4b0eee85
nabil24si <nabil24si@mahasiswa.pcr.ac.id>
2026-03-30 10:02:06 +0700

    Pertemuan 3

---
+++ app/src/main/java/com/example/nabilapps/pertemuan_3/ThirdActivity.kt
@@ -0,0 +1,35 @@
+package com.example.nabilapps.pertemuan_3
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.nabilapps.R
+import com.example.nabilapps.databinding.ActivityThirdBinding
+
+class ThirdActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityThirdBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        binding = ActivityThirdBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+//        val noTujuan: EditText = findViewById(R.id.inputNoTujuan)
+//        val btnKirim: Button = findViewById(R.id.btnKirim)
+
+        binding.btnKirim.setOnClickListener {
+            val intent = Intent(this, ThirdResultActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
