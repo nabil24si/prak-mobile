ae0030be0b27938f76cbbfabbde11ccef0f23b0c
nabil24si <nabil24si@mahasiswa.pcr.ac.id>
2026-04-20 08:12:43 +0700

    Pertemuan 6 Halaman Login

---
+++ app/src/main/java/com/example/nabilapps/AuthActivity.kt
@@ -0,0 +1,46 @@
+package com.example.nabilapps
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.nabilapps.databinding.ActivityAuthBinding
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
+
+class AuthActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityAuthBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAuthBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        binding.btnLogin.setOnClickListener {
+            val username = binding.username.text.toString()
+            val password = binding.password.text.toString()
+
+            if (username.isNotEmpty() && password.isNotEmpty()) {
+                Toast.makeText(this, " $username Login Anda Berhasil !", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this, MainActivity::class.java)
+                intent.putExtra("username", binding.username.text.toString()) // Kuncinya adalah "username"
+                startActivity(intent)
+            } else {5
+                MaterialAlertDialogBuilder(this)
+                    .setTitle("Coba Lagi")
+                    .setMessage("Silahkan Coba lagi")
+                    .setPositiveButton("Ya") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
+            }
+        }
+    }
+}
--- app/src/main/java/com/example/nabilapps/MainActivity.kt
+++ app/src/main/java/com/example/nabilapps/MainActivity.kt
@@ -1,13 +1,16 @@
 package com.example.nabilapps
 
+import android.app.AlertDialog
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.nabilapps.databinding.ActivityMainBinding
 import com.example.nabilapps.pertemuan_4.FourthActivity
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -33,5 +36,20 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("age", 25)
             startActivity(intent)
         }
+        binding.btnLogout.setOnClickListener {
+            MaterialAlertDialogBuilder(this)
+                .setTitle("Konfirmasi")
+                .setMessage("Apakah Anda yakin ingin melanjutkan?")
+                .setPositiveButton("Ya") { dialog, _ ->
+                    dialog.dismiss()
+                    finish()
+                }
+                .setNegativeButton("Batal") { dialog, _ ->
+                    dialog.dismiss()
+                    Log.e("Info Dialog","Anda memilih Tidak!")
+                }
+                .show()
+
+            }
+        }
     }
-}
